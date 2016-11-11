251eeea1dc44b4588f26bee4311c0073db74d29d
Yurii Kushpit <yuriikushpit@gmail.com>
2016-11-11 21:36:58 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String... args){
+        List<Integer>list = new ArrayList<>();
+        list.add(2);//0
+        list.add(5);//1
+
+        boolean empty=list.isEmpty();
+
+        System.out.println(empty);
+        }
+    }
+
